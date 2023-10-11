34c4b1c885846c3a8663a29b988f9cfb28ffe9c9
xtractor07 <tryy.m3@gmail.com>
2023-10-11 13:17:11 +0530

    Initial Commit

---
+++ dice_app/dice_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dice_appApp.swift
+//  dice_app
+//
+//  Created by Kumar Aman on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct dice_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
